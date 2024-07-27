1592f343ffd3725bb8a981b2fe5b87a801038146
morrison <morrison.k@hpcnt.com>
2024-07-27 14:19:26 +0900

    Initail Commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react-hooks/exhaustive-deps": "error",
   },
 }
---
+++ src/apis/user.ts
@@ -0,0 +1,18 @@
+import { GetUserReponseType, GetUserRequestType, PatchMeRequestType } from '../schema/userSchema';
+import httpClient from '.';
+
+export const getMe = async (): Promise<GetUserReponseType> => {
+  const response = await httpClient.get('/users/me');
+  return response.data;
+};
+
+export const getUser = async (request: GetUserRequestType): Promise<GetUserReponseType> => {
+  const { id } = request;
+  const response = await httpClient.get(`/users/${id}`);
+  return response.data;
+};
+
+export const updateMe = async (request: PatchMeRequestType): Promise<GetUserReponseType> => {
+  const response = await httpClient.patch('/users/me', { ...request });
+  return response.data;
+};
