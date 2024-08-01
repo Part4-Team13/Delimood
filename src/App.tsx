a0ea2fa3276e8cbb05eda6faad7af18818edb03f
oh-chaeyeon <chaebread555@gmail.com>
2024-08-01 14:29:33 +0900

    FE-23 🔧기초세팅 에픽에 포함된 코드 사용하려고 가져옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react';
+import Signup from './pages/signup';
+import { MantineProvider } from '@mantine/core';
+import { Routes, Route } from 'react-router-dom';
+//import Login from './pages/login';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <MantineProvider>
+      <Routes>
+        <Route path='signup' element={<Signup />} />
+      </Routes>
+    </MantineProvider>
   );
 }
 
