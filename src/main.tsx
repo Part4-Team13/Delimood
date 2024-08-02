4ec3ce8b79bed2592318919b2f053a239dbe91fa
oh-chaeyeon <chaebread555@gmail.com>
2024-08-02 15:00:35 +0900

    FE-25 ✨: react-toastify 적용하기

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import '../global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
+        <ToastContainer />
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
