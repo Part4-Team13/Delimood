742d560a912265f37f200edf8bd9b9058942ae33
unknown <csyou1228@gmail.com>
2024-08-03 13:04:28 +0900

    FE-8 :sparkles: : 더보기 버튼 css 보완

--- src/components/ViewMore.tsx
+++ src/components/ViewMore.tsx
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren } from 'react';
+import plus from '../assets/ico_plus.svg';
 
 interface ViewMoreProps extends PropsWithChildren {
   onClick: () => void;
@@ -7,9 +8,14 @@ interface ViewMoreProps extends PropsWithChildren {
 
 function ViewMore({ onClick, text }: ViewMoreProps) {
   return (
-    <button onClick={onClick} className='flex items-center gap-[4px] rounded-[100px] bg-background border-[1px] border-line-darker'>
-      <span className='text-blue-400 text-md'>+</span>
-      <span className='text-blue-400 text-md'>{text}</span>
+    <button
+      onClick={onClick}
+      className='flex items-center gap-[4px] rounded-[100px] bg-background border-[1px] border-line-darker p-[12px_18px] desktop:[12px_40px] text-md desktop:text-xl text-blue-400'
+    >
+      <span>
+        <img src={plus} alt='더보기' />
+      </span>
+      <span>{text}</span>
     </button>
   );
 }
