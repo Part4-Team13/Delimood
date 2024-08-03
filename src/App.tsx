e49ece10b8397f7bcc0fb83a96ad120526b0a714
oh-chaeyeon <chaebread555@gmail.com>
2024-08-03 14:06:18 +0900

    FE-25 🎨 : Mantine 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,18 @@ import Epigrams from './pages/epigrams';
 import Search from './pages/search';
 import Addepigram from './pages/addepigram';
 import Mypage from './pages/mypage';
-import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
-    <MantineProvider>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='signup' element={<Signup />} />
-        <Route path='login' element={<Login />} />
-        <Route path='epigrams' element={<Epigrams />} />
-        <Route path='search' element={<Search />} />
-        <Route path='addepigram' element={<Addepigram />} />
-        <Route path='mypage' element={<Mypage />} />
-      </Routes>
-    </MantineProvider>
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='signup' element={<Signup />} />
+      <Route path='login' element={<Login />} />
+      <Route path='epigrams' element={<Epigrams />} />
+      <Route path='search' element={<Search />} />
+      <Route path='addepigram' element={<Addepigram />} />
+      <Route path='mypage' element={<Mypage />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import '../global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -15,8 +16,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
-        <ToastContainer autoClose={3000} />
+        <MantineProvider>
+          <App />
+          <ToastContainer autoClose={3000} />
+        </MantineProvider>
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
