ebb5c756448d4cdcdb6119642eb2e28625f76ca8
unknown <csyou1228@gmail.com>
2024-08-03 19:33:49 +0900

    FE-8 :sparkles: : 더 불러오기 구현

--- src/components/EpigramCard.tsx
+++ src/components/EpigramCard.tsx
@@ -1,26 +1,26 @@
-interface EpigramCardProps {
-  author: string;
-  content: string;
-  tags: string[];
-  isSeperated: boolean;
-}
+import React from 'react';
+import { EpigramListType } from '../schema/epigram/EpigramGet';
 
-const EpigramCard = ({ author, content, tags, isSeperated }: EpigramCardProps) => {
+type EpigramCardProps = Pick<EpigramListType, 'id' | 'author' | 'content' | 'tags' | 'likeCount'> & { isSeperated: boolean };
+
+function EpigramCard({ id, author, content, tags, isSeperated }: EpigramCardProps) {
   return (
     <>
       <div className={`font-paraph text-md tablet:text-lg desktop:text-2xl ${isSeperated ? 'w-[152px] tablet:w-[294px] desktop:w-[585px]' : 'w-[312px] tablet:w-[384px] desktop:w-[640px]'}`}>
         <div className={`rounded-[16px] p-[23px] flex flex-col overflow-hidden bg-white ${isSeperated ? 'h-[110px] tablet:h-[180px] desktop:h-[259px] justify-between' : ''}`}>
-          <p className='text-black-600'>{content}</p>
+          <p className='text-black-600'>{`${content} - ${id}`}</p>
           <span className='ml-auto text-blue-400'>- {author} -</span>
         </div>
         <ul className='flex flex-row-reverse gap-[8px] text-blue-400'>
-          {tags.map((tag, index) => (
-            <li key={index}>#{tag}</li>
+          {tags.map((tag) => (
+            <li key={tag.id}>#{tag.name}</li>
           ))}
         </ul>
       </div>
     </>
   );
-};
+}
 
-export default EpigramCard;
+const MemoizedCard = React.memo(EpigramCard);
+
+export default MemoizedCard;
--- src/components/EpigramList.tsx
+++ src/components/EpigramList.tsx
@@ -1,41 +1,49 @@
-import { PropsWithChildren } from 'react';
+import { useEffect, useState } from 'react';
 import EpigramCard from './EpigramCard';
 import ViewMore from './ViewMore';
+import { useGetEpigrams } from '../hooks/useGetEpigrams';
+import { EpigramListType } from '../schema/epigram/EpigramGet';
 
-interface EpigramListProps extends PropsWithChildren {
+interface EpigramListProps {
   //  isMine: boolean;
   isWide?: boolean;
 }
 
-const mockDatas = [
-  {
-    id: 1,
-    author: '니체',
-    content: '신은 죽었다... 없다! 진짜 없다. 제발 좀 알아라! 좀 더 길게 적기 위해 아무말이나 추가하겠다~! 진짜 할 말 없지만 점점점을 추가하기 위해.........',
-    tags: ['tag1', 'tag2'],
-  },
-  { id: 2, author: '소크라테스', content: '너 자신을 ALLA', tags: ['tag1', 'tag2'] },
-  { id: 3, author: '궁예', content: '누가 기침소리를 내었는가', tags: ['tag1', 'tag2'] },
-];
-
-const handleViewMore = () => {};
-
 function EpigramList({ isWide = false }: EpigramListProps) {
+  const [epigramList, setEpigramList] = useState<EpigramListType[]>([]);
+  const [nextCursor, setNextCursor] = useState<number | null>(null);
+  const { data } = useGetEpigrams(3, nextCursor);
+  useEffect(() => {
+    if (data) {
+      const epigrams = data.list;
+      console.log(data.nextCursor);
+      console.log(data.totalCount);
+      setEpigramList((prevList) => [...prevList, ...epigrams]);
+    }
+  }, [data]);
+
+  const handleViewMore = () => {
+    if (data && data.nextCursor) {
+      setNextCursor(data.nextCursor);
+    }
+  };
+
   return (
     <div className='flex flex-col gap-[72px] mx-auto'>
       <ul
         className={`mx-auto max-w-fit grid ${isWide ? 'grid-cols-2 gap-y-[16px] gap-x-[8px] tablet:gap-y-[24px] tablet:gap-x-[12px] desktop:gap-y-[40px] desktop:gap-x-[20px]' : 'grid-cols-1 gap-[16px]'}`}
       >
-        {mockDatas.map((data) => (
+        {epigramList.map((data) => (
           <li key={data.id}>
-            <EpigramCard author={data.author} content={data.content} tags={data.tags} isSeperated={isWide} />
+            <EpigramCard id={data.id} author={data.author} content={data.content} tags={data.tags} isSeperated={isWide} likeCount={data.likeCount} />
           </li>
         ))}
       </ul>
-
-      <div className='mx-auto'>
-        <ViewMore text={isWide ? '에피그램 더보기' : '더보기'} onClick={() => handleViewMore} />
-      </div>
+      {data?.nextCursor && (
+        <div className='mx-auto'>
+          <ViewMore text={isWide ? '에피그램 더보기' : '더보기'} onClick={handleViewMore} />
+        </div>
+      )}
     </div>
   );
 }
