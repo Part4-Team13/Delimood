69ecdf9b63a6e60af6b0ccbe320e5d7076b26cda
Choi <soyeongvely@gmail.com>
2024-08-05 16:30:33 +0900

    FE-49 ✨ : 스키마 export 추가

--- src/schema/epigramSchema.ts
+++ src/schema/epigramSchema.ts
@@ -3,10 +3,10 @@ import * as z from 'zod';
 /* Post 에피그램 작성 */
 
 // 공통 정의 (tag, id, author, content)
-const TagName = z.string().min(1).max(10);
-const Id = z.number().int().min(1);
-const Author = z.string().min(1).max(30);
-const Content = z.string().min(1).max(500);
+export const TagName = z.string().min(1).max(10);
+export const Id = z.number().int().min(1);
+export const Author = z.string().min(1).max(30);
+export const Content = z.string().min(1).max(500);
 
 //에피그램 작성 Request
 export const PostEpigramRequest = z.object({
@@ -39,7 +39,7 @@ export const PostEpigramResponse = z.object({
 // 에피그램 목록 조회
 
 // Epigram List 정의
-const GetEpigramList = z.object({
+export const GetEpigramList = z.object({
   likeCount: z.number(),
   tags: z
     .array(
@@ -99,14 +99,14 @@ export const DeleteResponse = z.object({
 
 /* Get 에피그램 댓글 목록 조회 */
 // 댓글 작성자 정의
-const CommentWriter = z.object({
+export const CommentWriter = z.object({
   image: z.string().nullable(),
   nickname: z.string(),
   id: Id,
 });
 
 // 댓글 정의
-const Comment = z.object({
+export const Comment = z.object({
   epigramId: Id,
   writer: CommentWriter,
   updatedAt: z.string(),
@@ -117,13 +117,23 @@ const Comment = z.object({
 });
 
 // 페이지네이션 응답 정의
-export const CursorBasedPaginationResponse_CommentType = z.object({
-  totalCount: z.number(), // 전체 댓글 수
-  nextCursor: z.number().nullable(), // 다음 페이지의 커서, null을 허용
-  list: z.array(Comment), // 댓글 목록
+export const PaginationResponse = z.object({
+  totalCount: z.number(),
+  nextCursor: z.number().nullable(),
+  list: z.array(Comment),
 });
 
 /* 403, 404 에러 */
 export const ErrorResponse = z.object({
   message: z.string(),
 });
+
+export type PostEpigramRequestType = z.infer<typeof PostEpigramRequest>;
+export type PostEpigramResponseType = z.infer<typeof PostEpigramResponse>;
+export type GetEpigramListType = z.infer<typeof GetEpigramList>;
+export type GetEpigramListResponseType = z.infer<typeof GetEpigramListResponse>;
+export type EpigramDetailType = z.infer<typeof EpigramDetail>;
+export type UpdateEpigramRequestType = z.infer<typeof UpdateEpigramRequest>;
+export type DeleteResponseType = z.infer<typeof DeleteResponse>;
+export type PaginationResponseType = z.infer<typeof PaginationResponse>;
+export type ErrorResponseType = z.infer<typeof ErrorResponse>;
