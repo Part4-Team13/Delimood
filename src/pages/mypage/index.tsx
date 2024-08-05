5f836498b300acebb411447df5fe6c811221583b
oh-chaeyeon <chaebread555@gmail.com>
2024-08-05 16:20:18 +0900

    FE-25 ✨ : 마이페이지에 간단한 로그아웃 기능 추가

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,3 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { rem } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
+import { IconCheck } from '@tabler/icons-react';
+
 export default function Mypage() {
-  return <div>여기는 마이 페이지페이지입니다.</div>;
+  const checkIcon = <IconCheck style={{ width: rem(20), height: rem(20) }} />;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Cookies.remove('accessToken');
+    Cookies.remove('refreshToken');
+
+    showNotification({
+      title: '로그아웃',
+      message: '성공적으로 로그아웃되었습니다.',
+      icon: checkIcon,
+      color: 'teal',
+    });
+
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <p>여기는 마이 페이지입니다.</p>
+      <button onClick={handleLogout} className='h-[40px] w-[200px] bg-blue-default rounded-lg text-white'>
+        로그아웃
+      </button>
+    </div>
+  );
 }
