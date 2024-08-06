a88a568f6f7ef071b514671f32080c2fc09ce7fb
song-ari <sari0708@ajou.ac.kr>
2024-08-06 14:08:13 +0900

    FE-50 :recycle: EmotionLog API 관련 코드 전체적인 리팩토링

--- src/apis/emotionLog.ts
+++ src/apis/emotionLog.ts
@@ -1,17 +1,17 @@
-import { PostEmotionLog, GetTodayEmotionLog, GetMonthlyEmotionLogs } from '../schema/emotionLogSchema';
+import { EmotionLogRequestType, EmotionLogResponseType, EmotionLogsResponseType } from '../schema/emotionLogSchema';
 import httpClient from '.';
 
-export const postEmotionLog = async (request: PostEmotionLog): Promise<GetTodayEmotionLog> => {
+export const postEmotionLog = async (request: EmotionLogRequestType): Promise<EmotionLogResponseType> => {
   const response = await httpClient.post('/emotionLogs/today', request);
   return response.data;
 };
 
-export const getTodayEmotionLog = async (): Promise<GetTodayEmotionLog> => {
+export const getTodayEmotionLog = async (): Promise<EmotionLogResponseType> => {
   const response = await httpClient.get('/emotionLogs/today');
   return response.data;
 };
 
-export const getMonthlyEmotionLogs = async (): Promise<GetMonthlyEmotionLogs[]> => {
+export const getMonthlyEmotionLogs = async (): Promise<EmotionLogsResponseType> => {
   const response = await httpClient.get('/emotionLogs/monthly');
   return response.data;
 };
--- src/hooks/useEmotionLogQuery.ts
+++ src/hooks/useEmotionLogQuery.ts
@@ -1,14 +1,14 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import quries from '../apis/queries';
-import { PostEmotionLog, GetTodayEmotionLog } from '../schema/emotionLogSchema';
+import { EmotionLogRequestType, EmotionLogResponseType } from '../schema/emotionLogSchema';
 import { postEmotionLog } from '../apis/emotionLog';
 import { MutationOptions } from '../types/query';
 
 // 오늘의 감정 등록
-export const usePostEmotionLog = (options: MutationOptions<GetTodayEmotionLog>) => {
+export const usePostEmotionLog = (options: MutationOptions<EmotionLogResponseType>) => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: (request: PostEmotionLog) => postEmotionLog(request),
+    mutationFn: (request: EmotionLogRequestType) => postEmotionLog(request),
     ...options,
     onSuccess: (data, variables, context) => {
       queryClient.invalidateQueries(quries.emotionLogs.today());
@@ -38,4 +38,4 @@ export const useGetMonthlyEmotionLogs = () => {
   return useQuery(quries.emotionLogs.monthly());
 };
 // NOTE: 사용 방법
-// const { data, error, isLoading } = useGetTodayEmotionLog();
+// const { data, error, isLoading } = useGetMonthlyEmotionLog();
