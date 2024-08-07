1d2af3af80d87c1933522047f4f1aa97b0faaf35
Choi <soyeongvely@gmail.com>
2024-08-08 01:07:27 +0900

    FE-64 ✨ : 에피그램 만들기 버튼 & 상단으로 이동 버튼

---
+++ src/components/FixedButton.tsx
@@ -0,0 +1,53 @@
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import plus from '../assets/ico_plus.svg';
+import arrowUp from '../assets/ico_arrow_up.svg';
+
+interface ButtonProps {
+  text?: string;
+}
+
+function FixedButton({ text = '에피그램 만들기' }: ButtonProps) {
+  const navigate = useNavigate();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      //NOTE: 현재 스크롤 위치가 100px 이상일 경우 버튼 생성
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    navigate('/addepigram');
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className={`fixed bottom-20 right-[120px] flex flex-col items-end gap-4 transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+      <button onClick={handleClick} className='bg-blue-900 w-[194px] h-[64px] flex items-center justify-center text-white text-xl font-semibold py-2 px-4 rounded-full hover:bg-blue-800'>
+        <img src={plus} alt='Plus Icon' className='mr-2 w-6 h-6' />
+        {text}
+      </button>
+      <button onClick={scrollTop} className='bg-blue-900 text-white w-[64px] h-[64px] flex items-center justify-center rounded-full shadow-lg hover:bg-blue-800'>
+        <img src={arrowUp} alt='Arrow Up Icon' className='w-6 h-6' />
+      </button>
+    </div>
+  );
+}
+
+export default FixedButton;
