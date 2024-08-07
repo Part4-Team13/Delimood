56724d82de5df0008aca662ee22f5779280753ab
User850413 <csyou1228@gmail.com>
2024-08-07 15:53:19 +0900

    FE-6 :lipstick: : 프로필이미지 관련 css 보완

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -2,16 +2,16 @@ import CommentCard from './CommentCard';
 
 interface CommentListProps {
   // id: number;
-  isInfiniteScroll: boolean;
+  isInfiniteScroll?: boolean;
 }
 
 const MockData = [
-  { id: 1, updatedAt: 1, content: '이 글을 보니 힘이 납니다! 잘 하셨습니다!', writer: { image: '이미지', nickname: '작성자' } },
-  { id: 2, updatedAt: 3, content: '이 글을 보니 힘이 빠집니다.. 실수 하셨습니다!', writer: { image: '이미지2', nickname: '관찰자' } },
-  { id: 3, updatedAt: 6, content: '너무 더워.. 너무!!', writer: { image: '이미지2', nickname: '관찰자' } },
+  { id: 1, updatedAt: 1, content: '이 글을 보니 힘이 납니다! 잘 하셨습니다!', writer: { image: 'https://picsum.photos/200/300', nickname: '작성자' } },
+  { id: 2, updatedAt: 3, content: '이 글을 보니 힘이 빠집니다.. 실수 하셨습니다!', writer: { image: 'https://picsum.photos/200/300', nickname: '관찰자' } },
+  { id: 3, updatedAt: 6, content: '너무 더워.. 너무!!', writer: { image: 'https://picsum.photos/200/300', nickname: '관찰자' } },
 ];
 
-function CommentList({ isInfiniteScroll }: CommentListProps) {
+function CommentList({ isInfiniteScroll = false }: CommentListProps) {
   return (
     <div className='bg-background'>
       <ul className='mx-auto bg-yellow-300 w-fit'>
