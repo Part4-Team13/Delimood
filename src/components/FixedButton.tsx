f4b359c1f72498e0d067030b35c183d850fe68b3
Choi <soyeongvely@gmail.com>
2024-08-12 11:26:05 +0900

    FE-64 💄 :  반응형 추가

--- src/components/FixedButton.tsx
+++ src/components/FixedButton.tsx
@@ -38,16 +38,19 @@ function FixedButton({ text = '에피그램 만들기' }: ButtonProps) {
   };
 
   return (
-    <div className={`fixed bottom-20 right-[120px] flex flex-col items-end gap-4 transition-opacity duration-300`}>
+    <div className={`fixed bottom-20 flex flex-col items-end gap-4 transition-opacity duration-300 right-[40px] tablet:right-[72px] desktop:right-[120px]`}>
       {/* 기본으로 따라다니는 에피그램 만들기 버튼 */}
-      <button onClick={handleClick} className='bg-blue-900 w-[194px] h-[64px] flex items-center justify-center text-white text-xl font-semibold py-2 px-4 rounded-full hover:bg-blue-800'>
-        <img src={plus} alt='Plus Icon' className='mr-2 w-6 h-6' />
+      <button
+        onClick={handleClick}
+        className='bg-blue-900 flex items-center justify-center text-white  font-semibold rounded-full hover:bg-blue-800 text-md desktop:text-xl w-[145px] h-[48px] desktop:w-[194px] desktop:h-[64px]'
+      >
+        <img src={plus} alt='Plus Icon' className='mr-1 w-6 h-6' />
         {text}
       </button>
 
       {/* 스크롤 버튼은 스크롤 시에만 보이게 */}
       {isScrollButtonVisible && (
-        <button onClick={scrollTop} className='bg-blue-900 text-white w-[64px] h-[64px] flex items-center justify-center rounded-full shadow-lg hover:bg-blue-800'>
+        <button onClick={scrollTop} className='bg-blue-900 text-white w-[48px] h-[48px] desktop:w-[64px] desktop:h-[64px] flex items-center justify-center rounded-full shadow-lg hover:bg-blue-800'>
           <img src={arrowUp} alt='Arrow Up Icon' className='w-6 h-6' />
         </button>
       )}
