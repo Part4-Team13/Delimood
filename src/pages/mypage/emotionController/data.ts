663d04082336560adc688329e11068bacdbd3dd8
oh-chaeyeon <chaebread555@gmail.com>
2024-08-14 01:30:47 +0900

    FE-39 ✨ 데이터 더미 파일 생성

---
+++ src/pages/mypage/emotionController/data.ts
@@ -0,0 +1,50 @@
+export const rawData = [
+  {
+    id: 9085,
+    userId: 110,
+    emotion: 'SAD',
+    createdAt: '2024-08-22T00:00:00.000Z',
+  },
+  {
+    id: 9086,
+    userId: 110,
+    emotion: 'HAPPY',
+    createdAt: '2024-08-07T00:00:00.000Z',
+  },
+  {
+    id: 9087,
+    userId: 110,
+    emotion: 'HAPPY',
+    createdAt: '2024-08-08T00:00:00.000Z',
+  },
+  {
+    id: 9088,
+    userId: 110,
+    emotion: 'HAPPY',
+    createdAt: '2024-08-09T00:00:00.000Z',
+  },
+  {
+    id: 9089,
+    userId: 110,
+    emotion: 'ANGRY',
+    createdAt: '2024-08-10T00:00:00.000Z',
+  },
+  {
+    id: 9090,
+    userId: 110,
+    emotion: 'SAD',
+    createdAt: '2024-08-11T00:00:00.000Z',
+  },
+  {
+    id: 9091,
+    userId: 110,
+    emotion: 'THINKING',
+    createdAt: '2024-08-12T00:00:00.000Z',
+  },
+  {
+    id: 9092,
+    userId: 110,
+    emotion: 'MOVED',
+    createdAt: '2024-08-19T00:00:00.000Z',
+  },
+];
