91c02aa5c6c49dfb2c4893b983b28e4eb65e9398
User850413 <csyou1228@gmail.com>
2024-08-13 20:40:38 +0900

    FE-6 :sparkles: : useInfiniteQuery - useEpigramCommentsInfiniteQuery 추가

--- src/hooks/useInfiniteQuery.ts
+++ src/hooks/useInfiniteQuery.ts
@@ -2,11 +2,21 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import quries from '../apis/queries';
 import { GetCommentsRequestType } from '../schema/commentSchema';
 import { getComments } from '../apis/comment';
+import { getCommentList } from '../apis/epigram';
 
-export const useGetCommentsInfiniteQuery = (params: GetCommentsRequestType) => {
+export const useGetAllCommentsInfiniteQuery = (params: GetCommentsRequestType) => {
   return useInfiniteQuery({
     queryKey: quries.comments.getComments(params).queryKey,
     queryFn: ({ pageParam = 1 }) => getComments({ ...params, cursor: pageParam }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextCursor || null,
+  });
+};
+
+export const useEpigramCommentsInfiniteQuery = (id: number, params: GetCommentsRequestType) => {
+  return useInfiniteQuery({
+    queryKey: quries.epigrams.comments(id, params).queryKey,
+    queryFn: ({ pageParam = 1 }) => getCommentList(id, { ...params, cursor: pageParam }),
     initialPageParam: 0,
     getNextPageParam: (lastPage) => lastPage.nextCursor || null,
   });
