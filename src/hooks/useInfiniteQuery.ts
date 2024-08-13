affa30cc5cc046a865c45677307e66388df4ae47
User850413 <csyou1228@gmail.com>
2024-08-13 21:48:38 +0900

    FE-6 :sparkles: : useInfinitehook 수정

--- src/hooks/useInfiniteQuery.ts
+++ src/hooks/useInfiniteQuery.ts
@@ -1,10 +1,10 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import quries from '../apis/queries';
-import { GetCommentsRequestType } from '../schema/commentSchema';
+import { PaginationRequest } from '../schema/epigramSchema';
 import { getComments } from '../apis/comment';
 import { getCommentList } from '../apis/epigram';
 
-export const useGetAllCommentsInfiniteQuery = (params: GetCommentsRequestType) => {
+export const useGetAllCommentsInfiniteQuery = (params: PaginationRequest) => {
   return useInfiniteQuery({
     queryKey: quries.comments.getComments(params).queryKey,
     queryFn: ({ pageParam = 1 }) => getComments({ ...params, cursor: pageParam }),
@@ -13,7 +13,7 @@ export const useGetAllCommentsInfiniteQuery = (params: GetCommentsRequestType) =
   });
 };
 
-export const useEpigramCommentsInfiniteQuery = (id: number, params: GetCommentsRequestType) => {
+export const useEpigramCommentsInfiniteQuery = (id: number, params: PaginationRequest) => {
   return useInfiniteQuery({
     queryKey: quries.epigrams.comments(id, params).queryKey,
     queryFn: ({ pageParam = 1 }) => getCommentList(id, { ...params, cursor: pageParam }),
