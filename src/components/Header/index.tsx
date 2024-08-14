893bb71b4b382abef0f87e34d898c2577177688a
User850413 <csyou1228@gmail.com>
2024-08-14 21:54:18 +0900

    FE-18 :lipstick: : 변경된 디자인 반영

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -82,7 +82,7 @@ const HeaderRight: React.FC<HeaderButtonsProps> = ({ pathNow, navigate }) => {
     return (
       <button className='flex items-center gap-[6px] ml-auto' onClick={() => navigate('/mypage')}>
         <img className='w-[25px] h-[25px] tablet:w-[30px] tablet:h-[30px] object-cover rounded-full' src={src} alt={pathNow} />
-        <span className='hidden tablet:inline'>{data.nickname}</span>
+        <span className='hidden tablet:inline text-gray-300 text-sm desktop:text-md'>{data.nickname}</span>
       </button>
     );
   }
