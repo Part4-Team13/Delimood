bc5fec5ffc5e93dc9b6250d5098e23fa411b4b68
Choi <soyeongvely@gmail.com>
2024-08-19 22:40:24 +0900

    FE-66💄 : 브라우저 배경 색 높이 수정

--- src/pages/addepigram/index.tsx
+++ src/pages/addepigram/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useForm, isNotEmpty, hasLength } from '@mantine/form';
-import { Button, Group, TextInput, Input, Text, Textarea, Radio } from '@mantine/core';
+import { Button, Group, TextInput, Input, Text, Textarea, Radio, Divider } from '@mantine/core';
 import HashTag from '../../components/HashTag';
 
 export default function Demo() {
@@ -61,122 +61,124 @@ export default function Demo() {
   };
 
   return (
-    <div className=' flex items-center justify-center bg-white'>
-      <form onSubmit={form.onSubmit(() => {})} className='tablet:w-[384px] desktop:w-[640px] w-[312px] vertical-align'>
-        <div className='desktop:text-2xl tablet:text-xl  text-lg font-semibold mb-4 mt-[56px]'>에피그램 만들기</div>
+    <div className='w-[100vw] h-[100vh] bg-white'>
+      <div className='flex items-center justify-center'>
+        <form onSubmit={form.onSubmit(() => {})} className='tablet:w-[384px] desktop:w-[640px] w-[312px] vertical-align '>
+          <div className='desktop:text-2xl tablet:text-xl  text-lg font-semibold mb-4 mt-[56px]'>에피그램 만들기</div>
 
-        <Textarea
-          label='내용'
-          placeholder='500자 이내로 입력해주세요.'
-          withAsterisk
-          {...form.getInputProps('content')}
-          classNames={{
-            input: `desktop:text-xl desktop:w-[640px] desktop:h-[148px] tablet:w-[384px] tablet:h-[132px] w-[312px] h-[132px] rounded-[12px] mt-[24px] py-[10px] px-[16px]  desktop:placeholder:text-xl placeholder:text-lg `,
-            label: 'desktop:text-xl tablet:text-lg text-md mt-[40px]',
-            error: 'text-state-alert text-state-alert desktop:text-lg text-sm mt-1 float-right',
-          }}
-        />
-
-        <Input.Wrapper
-          label='저자'
-          withAsterisk
-          error={!form.errors.author && (form.values.author === '알 수 없음' || form.values.author === '본인') ? null : form.errors.author}
-          classNames={{
-            label: 'desktop:text-xl tablet:text-lg text-md mt-[54px]',
-            error: 'text-state-alert text-state-alert desktop:text-lg text-sm mt-1 float-right',
-          }}
-        >
-          <Radio.Group defaultValue='직접 입력' onChange={handleRadioChange}>
-            <Group mt='xl'>
-              <Radio
-                value='직접 입력'
-                label='직접 입력'
-                classNames={{
-                  label: 'desktop:text-xl text-lg desktop:mt-[-6px] mt-[-5px] ',
-                }}
-              />
-              <Radio
-                value='알 수 없음'
-                label='알 수 없음'
-                classNames={{
-                  label: 'desktop:text-xl text-lg desktop:mt-[-6px] mt-[-5px]',
-                }}
-              />
-              <Radio
-                value='본인'
-                label='본인'
-                classNames={{
-                  label: 'desktop:text-xl text-lg desktop:mt-[-6px] mt-[-5px]',
-                }}
-              />
-            </Group>
-          </Radio.Group>
-
-          <Input
-            placeholder={placeholder}
-            mt='md'
-            value={form.values.author}
-            {...form.getInputProps('author')}
-            onChange={(e) => form.setFieldValue('author', e.currentTarget.value)}
-            disabled={disabled}
+          <Textarea
+            label='내용'
+            placeholder='500자 이내로 입력해주세요.'
+            withAsterisk
+            {...form.getInputProps('content')}
             classNames={{
-              input:
-                'desktop:w-[640px] desktop:h-[64px] desktop:placeholder:text-xl desktop:text-xl text-lg placeholder:text-lg rounded-[12px] mt-[24px] py-[0px] px-[16px] desktop:placeholder:text-xl tablet:w-[384px] tablet:h-[44px] w-[312px] h-44px',
+              input: `desktop:text-xl desktop:w-[640px] desktop:h-[148px] tablet:w-[384px] tablet:h-[132px] w-[312px] h-[132px] rounded-[12px] mt-[24px] py-[10px] px-[16px]  desktop:placeholder:text-xl placeholder:text-lg `,
+              label: 'desktop:text-xl tablet:text-lg text-md mt-[40px]',
+              error: 'text-state-alert text-state-alert desktop:text-lg text-sm mt-1 float-right',
             }}
           />
-        </Input.Wrapper>
 
-        <TextInput
-          label='출처'
-          placeholder='출처 제목 입력'
-          mt='md'
-          {...form.getInputProps('source')}
-          classNames={{
-            input:
-              'desktop:text-xl  desktop:w-[640px] desktop:h-[64px] rounded-[12px] mt-[24px] py-[0px] px-[16px] placeholder:text-lg desktop:placeholder:text-xl tablet:w-[384px] tablet:h-[44px] w-[312px] h-44px text-lg ',
-            label: 'desktop:text-xl tablet:text-lg text-md mt-[54px]',
-          }}
-        />
-
-        <Input
-          placeholder='URL (ex. https://www.website.com)'
-          mt='md'
-          {...form.getInputProps('source')}
-          classNames={{
-            input:
-              'desktop:text-xl  desktop:w-[640px] desktop:h-[64px] rounded-[12px] mt-[24px] py-[0px] px-[16px] placeholder:text-lg desktop:placeholder:text-xl tablet:w-[384px] tablet:h-[44px] w-[312px] h-44px text-lg',
-          }}
-        />
-
-        <Text className='desktop:text-xl tablet:text-lg text-md mt-[54px]'>태그</Text>
-        <div className='mt-4'>
-          <Input
-            placeholder='입력하여 태그 작성 (최대 10자)'
-            value={newTag}
-            onChange={(e) => setNewTag(e.currentTarget.value)}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                e.preventDefault();
-                addTag();
-              }
+          <Input.Wrapper
+            label='저자'
+            withAsterisk
+            error={!form.errors.author && (form.values.author === '알 수 없음' || form.values.author === '본인') ? null : form.errors.author}
+            classNames={{
+              label: 'desktop:text-xl tablet:text-lg text-md mt-[54px]',
+              error: 'text-state-alert text-state-alert desktop:text-lg text-sm mt-1 float-right',
             }}
+          >
+            <Radio.Group defaultValue='직접 입력' onChange={handleRadioChange}>
+              <Group mt='xl'>
+                <Radio
+                  value='직접 입력'
+                  label='직접 입력'
+                  classNames={{
+                    label: 'desktop:text-xl text-lg desktop:mt-[-6px] mt-[-5px] ',
+                  }}
+                />
+                <Radio
+                  value='알 수 없음'
+                  label='알 수 없음'
+                  classNames={{
+                    label: 'desktop:text-xl text-lg desktop:mt-[-6px] mt-[-5px]',
+                  }}
+                />
+                <Radio
+                  value='본인'
+                  label='본인'
+                  classNames={{
+                    label: 'desktop:text-xl text-lg desktop:mt-[-6px] mt-[-5px]',
+                  }}
+                />
+              </Group>
+            </Radio.Group>
+
+            <Input
+              placeholder={placeholder}
+              mt='md'
+              value={form.values.author}
+              {...form.getInputProps('author')}
+              onChange={(e) => form.setFieldValue('author', e.currentTarget.value)}
+              disabled={disabled}
+              classNames={{
+                input:
+                  'desktop:w-[640px] desktop:h-[64px] desktop:placeholder:text-xl desktop:text-xl text-lg placeholder:text-lg rounded-[12px] mt-[24px] py-[0px] px-[16px] desktop:placeholder:text-xl tablet:w-[384px] tablet:h-[44px] w-[312px] h-44px',
+              }}
+            />
+          </Input.Wrapper>
+
+          <TextInput
+            label='출처'
+            placeholder='출처 제목 입력'
+            mt='md'
+            {...form.getInputProps('source')}
+            classNames={{
+              input:
+                'desktop:text-xl  desktop:w-[640px] desktop:h-[64px] rounded-[12px] mt-[24px] py-[0px] px-[16px] placeholder:text-lg desktop:placeholder:text-xl tablet:w-[384px] tablet:h-[44px] w-[312px] h-44px text-lg ',
+              label: 'desktop:text-xl tablet:text-lg text-md mt-[54px]',
+            }}
+          />
+
+          <Input
+            placeholder='URL (ex. https://www.website.com)'
+            mt='md'
+            {...form.getInputProps('source')}
             classNames={{
               input:
                 'desktop:text-xl  desktop:w-[640px] desktop:h-[64px] rounded-[12px] mt-[24px] py-[0px] px-[16px] placeholder:text-lg desktop:placeholder:text-xl tablet:w-[384px] tablet:h-[44px] w-[312px] h-44px text-lg',
             }}
           />
-          <HashTag tags={tags} removeTag={removeTag} />
-        </div>
 
-        <Group justify='flex-center' mt='md'>
-          <Button
-            type='submit'
-            className='desktop:w-[640px] desktop:h-[64px] desktop:text-xl tablet:w-[384px] tablet:h-[48px] w-[312px] h-[48px]  text-lg  rounded-xl bg-button-default hover:bg-button-hover mt-4 py-0 px-4'
-          >
-            작성 완료
-          </Button>
-        </Group>
-      </form>
+          <Text className='desktop:text-xl tablet:text-lg text-md mt-[54px]'>태그</Text>
+          <div className='mt-4'>
+            <Input
+              placeholder='입력하여 태그 작성 (최대 10자)'
+              value={newTag}
+              onChange={(e) => setNewTag(e.currentTarget.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                  addTag();
+                }
+              }}
+              classNames={{
+                input:
+                  'desktop:text-xl  desktop:w-[640px] desktop:h-[64px] rounded-[12px] mt-[24px] py-[0px] px-[16px] placeholder:text-lg desktop:placeholder:text-xl tablet:w-[384px] tablet:h-[44px] w-[312px] h-44px text-lg',
+              }}
+            />
+            <HashTag tags={tags} removeTag={removeTag} />
+          </div>
+
+          <Group justify='flex-center' mt='md'>
+            <Button
+              type='submit'
+              className='desktop:w-[640px] desktop:h-[64px] desktop:text-xl tablet:w-[384px] tablet:h-[48px] w-[312px] h-[48px]  text-lg  rounded-xl bg-button-default hover:bg-button-hover mt-4 py-0 px-4'
+            >
+              작성 완료
+            </Button>
+          </Group>
+        </form>
+      </div>
     </div>
   );
 }
