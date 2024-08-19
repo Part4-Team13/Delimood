a6acff47018b43a371851560adf228e1e16db4cb
Choi <soyeongvely@gmail.com>
2024-08-19 22:26:37 +0900

    FE-66 ✨ : 글 작성 페이지 UI 작업 및 반응형

--- src/components/HashTag.tsx
+++ src/components/HashTag.tsx
@@ -6,11 +6,11 @@ interface HashTagProps {
 function HashTag({ tags, removeTag }: HashTagProps) {
   return (
     <div className='flex flex-col gap-2 p-2'>
-      <div className='flex flex-wrap gap-2 mb-2'>
+      <div className='flex flex-wrap gap-2 mb-2 w-full max-w-[640px]'>
         {tags.map((tag, idx) => (
-          <div key={idx} className='flex items-center bg-background px-2 py-1 rounded-[22px] text-xl'>
-            <span className='p-1 text- tablet:text-xl desktop:text-2xl'>{tag}</span>
-            <button onClick={() => removeTag(idx)} className='ml-2 text-button-default'>
+          <div key={idx} className='flex items-center bg-background px-3 py-1 rounded-[22px] text-xl break-words max-w-full'>
+            <span className='p-1 text-lg tablet:text-xl desktop:text-2xl break-words'>{tag}</span>
+            <button onClick={() => removeTag(idx)} className='ml-2 text-button-default' style={{ cursor: 'pointer' }}>
               &times;
             </button>
           </div>
