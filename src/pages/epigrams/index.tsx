36d32c7cf4a8597f8f3690dbbe3572708a7198d8
oh-chaeyeon <chaebread555@gmail.com>
2024-08-19 22:12:31 +0900

    FE-27 ✨ : 오늘의 에피그램 구현

--- src/pages/epigrams/index.tsx
+++ src/pages/epigrams/index.tsx
@@ -1,3 +1,12 @@
+import TodayEpigram from './TodayEpigram';
+
 export default function Epigrams() {
-  return <div>여기는 메인 페이지입니다.</div>;
+  return (
+    <div className='mt-[32px] desktop:mt-[120px] flex flex-col items-center justify-center'>
+      <div className='flex flex-col gap-6 desktop:gap-10 w-[312px] tablet:w-[384px] desktop:w-[640px]'>
+        <span className='w-full text-base font-semibold justify-items-start text-black-600 desktop:text-2xl'>오늘의 에피그램</span>
+        <TodayEpigram />
+      </div>
+    </div>
+  );
 }
