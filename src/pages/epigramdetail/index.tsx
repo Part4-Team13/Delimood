c4fa0179424d0c7f849e4eb081c2766d1c230421
unknown <csyou1228@gmail.com>
2024-08-19 16:53:09 +0900

    FE-14 :sparkles: : 기본적인 UI 구축

--- src/pages/epigramdetail/index.tsx
+++ src/pages/epigramdetail/index.tsx
@@ -1,14 +1,60 @@
 import { useParams } from 'react-router-dom';
+import ico_like from '../../assets/ico_like.svg';
+import ico_external_link from '../../assets/ico_external_link.svg';
+import ico_more_vertical from '../../assets/ico_more_vertical.svg';
+import CommentList from '../../components/CommentList';
+import { useGetEpigramCommentsInfiniteQuery } from '../../hooks/useInfiniteQuery';
+import { useGetEpigramDetailQuery } from '../../hooks/useEpigramQuery';
 
 function EpigramDetail() {
   const { id } = useParams();
+  const epigramId: number = Number(id);
+  const { data: commentData, fetchNextPage, isFetching } = useGetEpigramCommentsInfiniteQuery(epigramId, { limit: 4 });
+  const { data, isLoading, isFetched } = useGetEpigramDetailQuery(epigramId);
 
-  return (
-    <div>
-      <h1>에피그램 상세페이지 </h1>
-      <p>에피그램 ID: {id}</p>
-    </div>
-  );
+  if (isLoading) {
+    return <div>로딩중입니다...</div>;
+  }
+  if (isFetched) {
+    return (
+      <>
+        <main className='w-[312px] tablet:w-[384px] desktop:w-[640px] mx-auto mb-[63px] tablet:mb-[87px] desktop:mb-[103px]'>
+          <div className='flex justify-between mt-[40px] text-lg desktop:text-xl'>
+            <ul className='flex gap-[16px] text-blue-400'>
+              {data!.tags.map((tag) => (
+                <li key={tag.id}>{tag.name}</li>
+              ))}
+            </ul>
+            <img src={ico_more_vertical} alt='에피그램 수정' className='w-[24px] cursor-pointer' />
+          </div>
+          <p className='text-2xl font-paraph my-[16px] tablet:my-[24px] desktop:my-[32px]'> {data!.content} </p>
+          <span className='text-blue-400 font-paraph block text-right text-lg tablet:text-xl desktop:text-2xl'>- {data!.author} -</span>
+          <ul className='flex gap-[8px] justify-center items-center mt-[32px] desktop:mt-[36px]'>
+            <li className='flex gap-[4px] bg-button-default hover:bg-button-hover text-white rounded-[100px] items-center h-fit p-[6px_14px] cursor-pointer'>
+              <img src={ico_like} alt='좋아요' className='w-[20px] desktop:w-[36px]' />
+              <span>{data!.likeCount} </span>
+            </li>
+            <li className='text-gray-300 bg-line-bright hover:bg-gray-100 rounded-[100px] flex items-center text-md desktop:text-xl p-[6px_14px] h-fit cursor-pointer'>
+              <span> {data!.referenceTitle}</span>
+              {data!.referenceUrl && (
+                <img
+                  src={ico_external_link}
+                  alt='새 창으로 이동'
+                  className='w-[20px] desktop:w-[36px]'
+                  onClick={() => {
+                    window.open(data!.referenceUrl!, '_blank');
+                  }}
+                />
+              )}
+            </li>
+          </ul>
+        </main>
+        <div>
+          <CommentList data={commentData} fetchNextPage={fetchNextPage} isFetching={isFetching} isInfiniteScroll />
+        </div>
+      </>
+    );
+  }
 }
 
 export default EpigramDetail;
