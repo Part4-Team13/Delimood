b7957d273b432f18a3376305779b90260f133e30
oh-chaeyeon <chaebread555@gmail.com>
2024-08-20 13:18:21 +0900

    FE-27 🚑️ : 조건에 맞게 댓글리스트 4개씩 보이게 설정

--- src/pages/epigrams/allCommentList.tsx
+++ src/pages/epigrams/allCommentList.tsx
@@ -4,7 +4,7 @@ import { useGetMeQuery } from '../../hooks/useUserQuery';
 
 export default function AllCommentList() {
   const { data: userData } = useGetMeQuery();
-  const { data, fetchNextPage, isFetching } = useGetAllCommentsInfiniteQuery({ limit: 3 });
+  const { data, fetchNextPage, isFetching } = useGetAllCommentsInfiniteQuery({ limit: 4 });
 
   //userData가 있을 경우에만 userId 전달하도록 설정
   return <CommentList data={data} fetchNextPage={fetchNextPage} isFetching={isFetching} isInfiniteScroll={false} buttonText='최신 댓글 더보기' {...(userData && { userId: userData.id })} />;
