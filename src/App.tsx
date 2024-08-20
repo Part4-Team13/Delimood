8052d1553395dd8bb57ff775df6d59388615ab12
User850413 <csyou1228@gmail.com>
2024-08-20 15:26:25 +0900

    FE-72 :sparkles: : 피드페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Addepigram from './pages/addepigram';
 import Mypage from './pages/mypage';
 import Layout from './layout/Layout';
 import EpigramDetail from './pages/epigramDetail';
+import EpigramBoard from './pages/epigramboard';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='search' element={<Search />} />
         <Route path='addepigram' element={<Addepigram />} />
         <Route path='mypage' element={<Mypage />} />
+        <Route path='board' element={<EpigramBoard />} />
       </Route>
     </Routes>
   );
