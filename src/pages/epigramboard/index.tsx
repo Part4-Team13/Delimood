7835fcc0d6e7395ee04a61856ad99a84c7b2ccbf
User850413 <csyou1228@gmail.com>
2024-08-20 21:55:43 +0900

    FE-72 :sparkles: : 버튼 컴포넌트 추가

--- src/pages/epigramboard/index.tsx
+++ src/pages/epigramboard/index.tsx
@@ -3,6 +3,7 @@ import EpigramList from '../../components/EpigramList';
 import { useGetEpigramListInfiniteQuery } from '../../hooks/useEpigramQuery';
 import ico_view_frames from '../../assets/ico_view_frames.svg';
 import ico_view_line from '../../assets/ico_view_line.svg';
+import FixedButton from '../../components/FixedButton';
 
 export default function EpigramBoard() {
   const [isWide, setIsWide] = useState<boolean>(false);
@@ -33,6 +34,7 @@ export default function EpigramBoard() {
 
   return (
     <>
+      <FixedButton />
       <div className='mt-[32px] desktop:mt-[120px] w-[312px] tablet:w-[600px] desktop:w-[1200px] mx-auto flex justify-between'>
         <span className='text-lg desktop:text-2xl font-bold'>피드</span>
         {showButton && (
