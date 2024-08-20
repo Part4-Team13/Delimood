391172f5c0d4ce53a6525c390ae99fae4231bd34
User850413 <csyou1228@gmail.com>
2024-08-20 15:44:04 +0900

    FE-72 :sparkles: : 피드 페이지 UI 구현

--- src/pages/epigramboard/index.tsx
+++ src/pages/epigramboard/index.tsx
@@ -1,3 +1,15 @@
+import EpigramList from '../../components/EpigramList';
+import { useGetEpigramListInfiniteQuery } from '../../hooks/useEpigramQuery';
+
 export default function EpigramBoard() {
-  return <></>;
+  const { data, fetchNextPage, isLoading } = useGetEpigramListInfiniteQuery({ limit: 6 });
+
+  return (
+    <>
+      <div className='text-lg desktop:text-2xl mt-[32px] desktop:mt-[120px] w-[312px] tablet:w-[600px] desktop:w-[1200px] mx-auto'>피드</div>
+      <div className='mt-[40px] mb-[114px]'>
+        <EpigramList data={data} fetchNextPage={fetchNextPage} isLoading={isLoading} isWide />
+      </div>
+    </>
+  );
 }
