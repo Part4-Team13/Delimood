0fcf456433fbbcd6b4766f614d40ffb824aad540
oh-chaeyeon <chaebread555@gmail.com>
2024-08-21 13:05:23 +0900

    FE-39 ✨ : 빈 댓글 , 빈 에피그램일때 화면추가

--- src/pages/mypage/MyCommentsList.tsx
+++ src/pages/mypage/MyCommentsList.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useGetMyCommentInfiniteQuery } from '../../hooks/useInfiniteQuery';
 import CommentList from '../../components/CommentList';
-
+import Search from '../../assets/ico_mypage_search.svg';
 interface MyCommentsListProps {
   userId: number;
   onTotalCountFetched: (totalCount: number) => void;
@@ -16,7 +17,37 @@ const MyCommentsList: React.FC<MyCommentsListProps> = ({ userId, onTotalCountFet
     }
   }, [data, onTotalCountFetched]);
 
-  return <CommentList data={data} fetchNextPage={fetchNextPage} isFetching={isFetching} userId={userId} isInfiniteScroll={false} buttonText='최신 댓글 더보기' />;
+  const isEmpty = data?.pages?.[0]?.totalCount === 0;
+
+  const navigate = useNavigate();
+  const onClickMyEpigramList = () => {
+    //Fix:board 경로로 수정예정
+    navigate(`/epigrams`);
+  };
+
+  return (
+    <div>
+      {isEmpty ? (
+        <div className='w-[312px] h-[304px] flex flex-col items-center justify-center gap-2 tablet:gap-4 desktop:gap-6 tablet:w-[384px] desktop:w-[640px] desktop:h-[488px]'>
+          <img src={Search} alt='돋보기' className='desktop:w-[144px]' />
+          <div className='flex-col flex items-center gap-8 tablet:gap-10 desktop:gap-12 text-sm font-normal desktop:text-xl'>
+            <span className='flex-col flex items-center'>
+              아직 작성한 댓글이 없어요! <p />
+              댓글을 달고 다른 사람들과 교류해보세요.
+            </span>
+            <button
+              onClick={onClickMyEpigramList}
+              className='border-gray-100 rounded-[100px] border px-[18px] py-[12px] desktop:px-[20px] text-black-400 hover:bg-black-950 hover:text-white focus:bg-black-950 focus:text-white'
+            >
+              에피그램 둘러보기
+            </button>
+          </div>
+        </div>
+      ) : (
+        <CommentList data={data} fetchNextPage={fetchNextPage} isFetching={isFetching} userId={userId} isInfiniteScroll={false} buttonText='최신 댓글 더보기' />
+      )}
+    </div>
+  );
 };
 
 export default MyCommentsList;
--- src/pages/mypage/MyEpigramList.tsx
+++ src/pages/mypage/MyEpigramList.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import EpigramList from '../../components/EpigramList';
+import { useNavigate } from 'react-router-dom';
 import { useGetEpigramListInfiniteQuery } from '../../hooks/useEpigramQuery';
+import Search from '../../assets/ico_mypage_search.svg';
 
 interface MyEpigramListProps {
   userId: number;
@@ -16,7 +18,36 @@ const MyEpigramList: React.FC<MyEpigramListProps> = ({ userId, onTotalCountFetch
     }
   }, [data, onTotalCountFetched]);
 
-  return <EpigramList data={data} fetchNextPage={fetchNextPage} isLoading={isFetching} isWide={false} />;
+  const isEmpty = data?.pages?.[0]?.totalCount === 0;
+
+  const navigate = useNavigate();
+  const onClickMyEpigramList = () => {
+    navigate(`/addepigram`);
+  };
+
+  return (
+    <div>
+      {isEmpty ? (
+        <div className='w-[312px] h-[304px] flex flex-col items-center justify-center gap-2 tablet:gap-4 desktop:gap-6 tablet:w-[384px] desktop:w-[640px] desktop:h-[488px]'>
+          <img src={Search} alt='돋보기' className='desktop:w-[144px]' />
+          <div className='flex-col flex items-center gap-8 tablet:gap-10 desktop:gap-12 text-sm font-normal desktop:text-xl'>
+            <span className='flex-col flex items-center'>
+              아직 작성한 에피그램이 없어요! <p />
+              에피그램을 작성하고 감정을 공유해보세요.
+            </span>
+            <button
+              onClick={onClickMyEpigramList}
+              className='border-gray-100 rounded-[100px] border px-[18px] py-[12px] desktop:px-[20px] text-black-400 hover:bg-black-950 hover:text-white focus:bg-black-950 focus:text-white'
+            >
+              에피그램 만들기
+            </button>
+          </div>
+        </div>
+      ) : (
+        <EpigramList data={data} fetchNextPage={fetchNextPage} isLoading={isFetching} isWide={false} />
+      )}
+    </div>
+  );
 };
 
 export default MyEpigramList;
