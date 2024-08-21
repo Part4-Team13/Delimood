9ef5177916a7160fe8f2d1b8672ea90f4a2f3da0
oh-chaeyeon <chaebread555@gmail.com>
2024-08-22 03:55:13 +0900

    FE-39 ✨ : 유저 데이터 받아서 넣어주기

--- src/pages/mypage/emotionController/index.tsx
+++ src/pages/mypage/emotionController/index.tsx
@@ -3,6 +3,7 @@ import dayjs from 'dayjs';
 import EmotionCalendar from './EmotionCalendar';
 import EmotionChart from './EmotionChart';
 import { useGetMonthlyEmotionLogs } from '../../../hooks/useEmotionLogQuery';
+import { useGetMeQuery } from '../../../hooks/useUserQuery';
 
 const today = dayjs();
 const initialYear = today.year();
@@ -11,17 +12,20 @@ const initialMonth = today.month() + 1;
 export default function EmotionController() {
   const [year, setYear] = useState<number>(initialYear);
   const [month, setMonth] = useState<number>(initialMonth);
-  const userId = 110;
+  const { data: userData, error: userError, isLoading: userLoading } = useGetMeQuery();
 
-  const { data, error, isLoading } = useGetMonthlyEmotionLogs({ userId, year, month });
+  const userId = userData?.id;
 
+  const { data, error, isLoading } = useGetMonthlyEmotionLogs({ userId: userId || 0, year, month });
+
+  if (userLoading) return <div className='min-h-[800px] flex items-center justify-center'>Loading...</div>;
+  if (userError) return <div>Error fetching user data</div>;
   if (isLoading) return <div className='min-h-[800px] flex items-center justify-center'>Loading...</div>;
-  if (error) return <div>Error</div>;
+  if (error) return <div>Error fetching emotion logs</div>;
 
   const handleDateChange = (newYear: number, newMonth: number) => {
     setYear(newYear);
     setMonth(newMonth);
-    console.log('Updated Year:', newYear, 'Updated Month:', newMonth);
   };
 
   return (
