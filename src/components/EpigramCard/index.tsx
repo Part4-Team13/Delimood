fef4f623c6a56e7d9fb8634fadff004b32ea44db
User850413 <csyou1228@gmail.com>
2024-08-21 20:22:51 +0900

    FE-75 :sparkles: : 에피그램 리스트 좋아요 기능 구현

--- src/components/EpigramCard/index.tsx
+++ src/components/EpigramCard/index.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import like from '../assets/ico_like.svg';
-import { GetEpigramListType } from '../schema/epigramSchema';
+import { GetEpigramListType } from '../../schema/epigramSchema';
+import LikeButton from './LikeButton';
 
 type EpigramCardProps = Pick<GetEpigramListType, 'id' | 'author' | 'content' | 'tags'> & { isSeperated: boolean };
-// type LikeButtonProps = Pick<EpigramCardProps, 'id'>;
-
-// 클릭 시 해당 epigram의 like +1
-const onClickLikeButton = () => {};
-
-const LikeButton: React.FC = () => {
-  return (
-    <button onClick={onClickLikeButton} className={`bg-button-diabled hover:bg-button-hover w-[20px] tablet:w-[24px] rounded-full p-[2px] absolute  right-[6px] top-[6px]`}>
-      {/* {${*isMine ? 'bg-button-default cursor-pointer hover:bg-button-hover' : 'bg-button-diabled cursor-default' }} */}
-      <img src={like} alt='좋아요' />
-    </button>
-  );
-};
 
 function EpigramCard({ id, author, content, tags, isSeperated = false }: EpigramCardProps) {
   const navigate = useNavigate();
-  const onClickEpigramCard = () => {
+  const onClickEpigramCard = (e: React.SyntheticEvent) => {
+    e.stopPropagation();
     navigate(`/epigrams/${id}`);
   };
 
   return (
     <>
       <div
-        onClick={onClickEpigramCard}
+        onClick={(e) => onClickEpigramCard(e)}
         className={`relative font-paraph text-md tablet:text-lg desktop:text-2xl ${isSeperated ? 'w-[152px] tablet:w-[294px] desktop:w-[585px]' : 'w-[312px] tablet:w-[384px] desktop:w-[640px]'}`}
       >
         <div
@@ -40,10 +28,12 @@ function EpigramCard({ id, author, content, tags, isSeperated = false }: Epigram
         </div>
         <ul className={`flex text-blue-400 ml-auto w-fit mt-[8px] ${isSeperated ? 'flex-col gap-0' : 'flex-row gap-[8px]'}`}>
           {tags.map((tag) => (
-            <li key={tag.id}>{tag.name}</li>
+            <li key={tag.id} className='cursor-default'>
+              {tag.name}
+            </li>
           ))}
         </ul>
-        <LikeButton />
+        <LikeButton id={id} />
       </div>
     </>
   );
