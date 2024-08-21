12c980ae9740c7fe68fc0e9b79a81907eaeee2b9
oh-chaeyeon <chaebread555@gmail.com>
2024-08-21 16:57:41 +0900

    FE-51 ✨ : 구글 간편가입 구현

---
+++ src/hooks/useGoogleAuth.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import useGoogleLogin from './useGoogleLogin';
+
+const useGoogleAuth = () => {
+  const location = useLocation();
+  const { mutate: googleLogin } = useGoogleLogin();
+
+  const params = new URLSearchParams(location.search);
+  const code = params.get('code');
+
+  useEffect(() => {
+    if (code) {
+      googleLogin(code);
+    }
+  }, [code, googleLogin]);
+
+  return null;
+};
+
+export default useGoogleAuth;
