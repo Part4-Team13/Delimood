66cb24ed22b2fdb00b25fc17411b90150d21495b
User850413 <csyou1228@gmail.com>
2024-08-21 16:04:37 +0900

    FE-14 :lipstick: : 노트 배경 수정

--- src/pages/epigramdetail/index.tsx
+++ src/pages/epigramdetail/index.tsx
@@ -13,7 +13,7 @@ import { usePostCommentMutation } from '../../hooks/useCommentQuery';
 import { showNotification } from '@mantine/notifications';
 import { IconX } from '@tabler/icons-react';
 import { useQueryClient } from '@tanstack/react-query';
-import img_bg_top from '../../assets/img_bg_top.png';
+import img_zigzag from '../../assets/img_zigzag.png';
 const xIcon = <IconX style={{ width: rem(20), height: rem(20) }} />;
 
 function EpigramDetail() {
@@ -22,7 +22,6 @@ function EpigramDetail() {
   const [isCommentPrivate, setIsCommentPrivate] = useState<boolean>(false);
   const [likeCount, setLikeCount] = useState<number>(0);
   const [like, setLike] = useState<boolean>(false);
-  const [backgroundSize, setBackgroundSize] = useState<string>('auto');
   const addComment = useRef<HTMLTextAreaElement | null>(null);
 
   const queryClient = useQueryClient();
@@ -33,24 +32,6 @@ function EpigramDetail() {
   const { data: commentData, fetchNextPage, isFetching } = useGetEpigramCommentsInfiniteQuery(epigramId, { limit: 4 });
   const { data, isLoading, isFetched } = useGetEpigramDetailQuery(epigramId);
   const navigate = useNavigate();
-
-  //NOTE : 배경 반응형 관련
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 744) {
-        setBackgroundSize('cover');
-      } else {
-        setBackgroundSize('contain');
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  });
 
   //NOTE : 좋아요 관련
   useEffect(() => {
@@ -153,54 +134,53 @@ function EpigramDetail() {
   if (isFetched) {
     return (
       <div className='overflow-hidden'>
-        <div
-          className='-z-10 absolute top-[-30px] desktop:top-[30px] left-0 right-0 min-h-[407px]'
-          style={{ backgroundImage: `url(${img_bg_top})`, backgroundSize: backgroundSize, backgroundPosition: 'top', backgroundRepeat: 'repeat-x' }}
-        />
-        <main className='w-[312px] tablet:w-[384px] desktop:w-[640px] mx-auto mb-[63px] tablet:mb-[87px] desktop:mb-[103px] mt-[40px]'>
-          <div className='flex justify-between text-lg desktop:text-xl'>
-            <ul className='flex gap-[16px] text-blue-400'>{data && data.tags.map((tag) => <li key={tag.id}>{tag.name}</li>)}</ul>
-            {userData?.id === data?.writerId && (
-              <Menu>
-                <Menu.Target>
-                  <button>
-                    <img src={ico_more_vertical} alt='에피그램 수정' className='w-[24px] cursor-pointer' />
-                  </button>
-                </Menu.Target>
-                <Menu.Dropdown className='bg-background rounded-[16px] border-[1px] border-blue-300'>
-                  <Menu.Item className='text-md desktop:text-xl p-[8px_24px] desktop:p-[12px_32px]' onClick={() => navigate(`/editepigram/${epigramId}`)}>
-                    수정하기
-                  </Menu.Item>
-                  <Menu.Item className='text-md desktop:text-xl p-[8px_24px] desktop:p-[12px_32px]' onClick={onClickDeleteEpigram}>
-                    삭제하기
-                  </Menu.Item>
-                </Menu.Dropdown>
-              </Menu>
-            )}
-          </div>
-          <p className='text-2xl font-paraph my-[16px] tablet:my-[24px] desktop:my-[32px]'> {data!.content} </p>
-          <span className='text-blue-400 font-paraph block text-right text-lg tablet:text-xl desktop:text-2xl'>- {data!.author} -</span>
-          <ul className='flex gap-[8px] justify-center items-center mt-[32px] desktop:mt-[36px]'>
-            <li>
-              <button
-                onClick={onClickLikeButton}
-                className={`flex gap-[4px] text-white rounded-[100px] items-center h-fit p-[6px_14px] cursor-pointer ${like ? 'bg-state-alert hover:bg-red' : 'bg-button-default hover:bg-button-hover'}`}
+        <main className='relative w-screen mb-[63px] tablet:mb-[87px] desktop:mb-[103px] pt-[40px] pb-[16px] tablet:pb-[32px] desktop:pb-[40px] striped desktop:striped-desktop shadow-epigramdetail'>
+          <div className='mx-auto w-fit'>
+            <div className='flex justify-between text-lg desktop:text-xl'>
+              <ul className='flex gap-[16px] text-blue-400'>{data && data.tags.map((tag) => <li key={tag.id}>{tag.name}</li>)}</ul>
+              {userData?.id === data?.writerId && (
+                <Menu>
+                  <Menu.Target>
+                    <button>
+                      <img src={ico_more_vertical} alt='에피그램 수정' className='w-[24px] cursor-pointer' />
+                    </button>
+                  </Menu.Target>
+                  <Menu.Dropdown className='bg-background rounded-[16px] border-[1px] border-blue-300'>
+                    <Menu.Item className='text-md desktop:text-xl p-[8px_24px] desktop:p-[12px_32px]' onClick={() => navigate(`/editepigram/${epigramId}`)}>
+                      수정하기
+                    </Menu.Item>
+                    <Menu.Item className='text-md desktop:text-xl p-[8px_24px] desktop:p-[12px_32px]' onClick={onClickDeleteEpigram}>
+                      삭제하기
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              )}
+            </div>
+            <p className='text-2xl font-paraph my-[16px] tablet:my-[24px] desktop:my-[32px] w-[312px] tablet:w-[384px] desktop:w-[640px]'>{data!.content}</p>
+            <span className='text-blue-400 font-paraph block text-right text-lg tablet:text-xl desktop:text-2xl'>- {data!.author} -</span>
+            <ul className='flex gap-[8px] justify-center items-center mt-[32px] desktop:mt-[36px]'>
+              <li>
+                <button
+                  onClick={onClickLikeButton}
+                  className={`flex gap-[4px] text-white rounded-[100px] items-center h-fit p-[6px_14px] cursor-pointer ${like ? 'bg-state-alert hover:bg-red' : 'bg-button-default hover:bg-button-hover'}`}
+                >
+                  <img src={ico_like} alt='좋아요' className='w-[20px] desktop:w-[36px]' />
+                  <span>{likeCount}</span>
+                </button>
+              </li>
+              <li
+                onClick={() => {
+                  window.open(data!.referenceUrl!, '_blank');
+                }}
               >
-                <img src={ico_like} alt='좋아요' className='w-[20px] desktop:w-[36px]' />
-                <span>{likeCount}</span>
-              </button>
-            </li>
-            <li
-              onClick={() => {
-                window.open(data!.referenceUrl!, '_blank');
-              }}
-            >
-              <button className='text-gray-300 bg-line-bright hover:bg-gray-100 rounded-[100px] flex items-center text-md desktop:text-xl p-[6px_14px] h-fit cursor-pointer'>
-                <span> {data!.referenceTitle}</span>
-                {data!.referenceUrl && <img src={ico_external_link} alt='새 창으로 이동' className='w-[20px] desktop:w-[36px]' />}
-              </button>
-            </li>
-          </ul>
+                <button className='text-gray-300 bg-line-bright hover:bg-gray-100 rounded-[100px] flex items-center text-md desktop:text-xl p-[6px_14px] h-fit cursor-pointer'>
+                  <span> {data!.referenceTitle}</span>
+                  {data!.referenceUrl && <img src={ico_external_link} alt='새 창으로 이동' className='w-[20px] desktop:w-[36px]' />}
+                </button>
+              </li>
+            </ul>
+          </div>
+          <span className='absolute bottom-[-15px] w-screen h-[15px]' style={{ backgroundImage: `url(${img_zigzag})`, backgroundRepeat: 'repeat-x' }} />
         </main>
         <div>
           {userData && (
