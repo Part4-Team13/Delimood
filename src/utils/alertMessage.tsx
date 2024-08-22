c7228b4f3a2c22fd39ec00ff6dbe609461f3326f
User850413 <csyou1228@gmail.com>
2024-08-22 16:25:41 +0900

    FE-78 :sparkles: : 알림메시지 따로 뺌

---
+++ src/utils/alertMessage.tsx
@@ -0,0 +1,36 @@
+import { rem } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
+import { IconX } from '@tabler/icons-react';
+
+const xIcon = <IconX style={{ width: rem(20), height: rem(20) }} />;
+
+interface AlertMessageProps {
+  title: string;
+  message: string;
+  color: 'dark' | 'gray' | 'red' | 'pink' | 'grape' | 'violet' | 'indigo' | 'blue' | 'cyan' | 'green' | 'lime' | 'yellow' | 'orange' | 'teal';
+}
+
+function alertMessage({ title = '알림 메시지입니다.', message = '부가 설명입니다.', color = 'dark' }: AlertMessageProps) {
+  const alert = showNotification({
+    title,
+    message,
+    icon: xIcon,
+    color,
+    autoClose: 2000,
+    styles: () => ({
+      root: {
+        position: 'fixed',
+        top: '10%',
+        right: '3%',
+        transform: 'translate(-50%, -50%)',
+        minWidth: '300px',
+        width: '40%',
+        maxWidth: '70%',
+      },
+    }),
+  });
+
+  return alert;
+}
+
+export default alertMessage;
