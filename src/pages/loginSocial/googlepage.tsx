990917dc118aff2a7bf9104f99ba5f21e6057628
oh-chaeyeon <chaebread555@gmail.com>
2024-08-22 18:56:26 +0900

    FE-76 💄 :  소셜로그인 로딩중페이지 UI 수정

--- src/pages/loginSocial/googlepage.tsx
+++ src/pages/loginSocial/googlepage.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import useGoogleAuth from '../../hooks/useGoogleAuth';
+import { Loader, Center, Text } from '@mantine/core';
 
 const GooglePage: React.FC = () => {
   useGoogleAuth();
 
-  return <div>로그인 중입니다...</div>;
+  return (
+    <Center style={{ height: '100vh', flexDirection: 'column' }}>
+      <Loader size={49} variant='bars' color='red' />
+      <Text mt='md' size='lg'>
+        구글 로그인중입니다.
+      </Text>
+    </Center>
+  );
 };
 
 export default GooglePage;
