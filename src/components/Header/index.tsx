eab7525837e126a55a9ed72d4ba09b553700bf08
User850413 <csyou1228@gmail.com>
2024-08-23 16:57:50 +0900

    FE-84 :lipstick: : header 컴포넌트 보여주는 방식 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie';
 import HeaderLeft from './HeaderLeft';
 import HeaderMiddle from './HeaderMiddle';
 import { HeaderRight, HeaderRightUnAuthenticated } from './HeaderRight';
+import { useEffect, useState } from 'react';
 
 export interface HeaderButtonsProps {
   pathNow: string;
@@ -10,12 +11,36 @@ export interface HeaderButtonsProps {
 }
 
 function Header() {
+  const [isScrollUp, setIsScrollUp] = useState<boolean>(true);
+
   const { pathname: pathNow } = useLocation();
   const navigate = useNavigate();
   const accessToken = Cookies.get('accessToken');
 
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleDetectScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY < lastScrollY) {
+        setIsScrollUp(true);
+      } else {
+        setIsScrollUp(false);
+      }
+      lastScrollY = currentScrollY;
+    };
+
+    window.addEventListener('scroll', handleDetectScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleDetectScroll);
+    };
+  }, []);
+
   return (
-    <div className='bg-white flex h-[52px] px-[24px] tablet:h-[60px] tablet:px-[72px] desktop:px-[120px] desktop:h-[80px] items-center fixed left-0 right-0 top-0 z-50 shadow-epigramdetail'>
+    <div
+      className={`bg-white flex h-[52px] px-[24px] tablet:h-[60px] tablet:px-[72px] desktop:px-[120px] desktop:h-[80px] items-center fixed left-0 right-0 z-50 shadow-epigramdetail transition-all ease-in-out delay-150 ${isScrollUp ? 'top-0' : 'top-[-52px] tablet:top-[-60px] desktop:top-[-80px]'}`}
+    >
       <HeaderLeft pathNow={pathNow} navigate={navigate} />
       <HeaderMiddle pathNow={pathNow} navigate={navigate} />
       {accessToken ? <HeaderRight pathNow={pathNow} navigate={navigate} /> : <HeaderRightUnAuthenticated pathNow={pathNow} navigate={navigate} />}
