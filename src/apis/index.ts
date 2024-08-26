d3b68db6ec006509b8743e1c893a86004150efff
oh-chaeyeon <chaebread555@gmail.com>
2024-08-26 18:19:46 +0900

    FE-87 ♻️ : 로그인 403 에러처리하기

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import qs from 'qs';
 import { REACT_APP_API_URL } from '../constants/env';
+import alertMessage from '../components/AlertMessage';
 
 const httpClient = axios.create({
   baseURL: REACT_APP_API_URL,
@@ -27,9 +28,10 @@ httpClient.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
+    const statusCode = error.response?.status;
 
-    // NOTE : 인증 오류 401 에러가 발생한 경우
-    if (error.response?.status === 401) {
+    // NOTE : 인증 오류 401, 403 에러가 발생한 경우
+    if (statusCode === 401 || statusCode === 403) {
       try {
         const refreshToken = localStorage.getItem('refreshToken');
         if (!refreshToken) throw new Error('refreshToken 문제발생!');
@@ -44,9 +46,10 @@ httpClient.interceptors.response.use(
         return httpClient(originalRequest);
       } catch (refreshError) {
         //Refactor : refreshToken이 만료되었거나 다른 오류 발생 시 로그아웃 처리, 알림 메시지 후 로그인 창으로 이동
-        console.error('Refresh token failed:', refreshError);
         localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
+        window.location.href = '/login';
+        alertMessage({ title: '로그인 기록이 만료되었습니다.', message: '다시 로그인 부탁드립니다.', color: 'red' });
       }
     }
 
