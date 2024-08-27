65542c649bc2dd640aa22639f53d232fea99425c
User850413 <csyou1228@gmail.com>
2024-08-27 14:39:48 +0900

    FE-91 :recycle: : LikeButton useEffect 삭제

--- src/components/Header/HeaderRight.tsx
+++ src/components/Header/HeaderRight.tsx
@@ -5,16 +5,13 @@ import user from '../../assets/ico_user.svg';
 // 로그인 O
 export function HeaderRight({ pathNow, navigate }: HeaderButtonsProps) {
   const { data } = useGetMeQuery();
-  if (data) {
-    const src = data.image ? data.image : user;
-    return (
-      <button className='flex items-center gap-[10px] ml-auto' onClick={() => navigate('/mypage')}>
-        <img className='w-[25px] h-[25px] tablet:w-[30px] tablet:h-[30px] object-cover rounded-full' src={src} alt={pathNow} />
-        <span className='hidden tablet:inline text-gray-300 text-sm desktop:text-md'>{data.nickname}</span>
-      </button>
-    );
-  }
-  return null;
+  const src = data.image ? data.image : user;
+  return (
+    <button className='flex items-center gap-[10px] ml-auto' onClick={() => navigate('/mypage')}>
+      <img className='w-[25px] h-[25px] tablet:w-[30px] tablet:h-[30px] object-cover rounded-full' src={src} alt={pathNow} />
+      <span className='hidden tablet:inline text-gray-300 text-sm desktop:text-md'>{data.nickname}</span>
+    </button>
+  );
 }
 
 // 로그인 X
