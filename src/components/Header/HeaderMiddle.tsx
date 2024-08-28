4b5118583766dea0d0526c5834fc5604ef493b99
oh-chaeyeon <chaebread555@gmail.com>
2024-08-28 19:22:54 +0900

    FE-28 💄 : 새로운 로고로 변경작업

--- src/components/Header/HeaderMiddle.tsx
+++ src/components/Header/HeaderMiddle.tsx
@@ -1,11 +1,11 @@
 import { HeaderButtonsProps } from '.';
-import logo from '../../assets/ico_logo.svg';
+import logo from '../../assets/ico_newLogo.svg';
 
 export default function HeaderMiddle({ pathNow, navigate }: HeaderButtonsProps) {
   if (!['/', '/login', '/signup'].includes(pathNow)) return null;
   return (
-    <button className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' onClick={() => navigate('/')}>
-      <img src={logo} alt='메인 페이지로' className='w-[101px] desktop:w-[130px]' />
+    <button className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' onClick={() => navigate('/')}>
+      <img src={logo} alt='메인 페이지로' className='w-[130px] desktop:w-[190px]' />
     </button>
   );
 }
